bc8a6eb736ad2b874938516dac5e483d0ad2b5ea
karasz <zsombfish@gmail.com>
2026-02-20 10:41:52 +0100

    Novenyek tesztelese

--- Uveghazrendszer/Adattar.cs
+++ Uveghazrendszer/Adattar.cs
@@ -6,5 +6,25 @@ namespace Uveghazrendszer
 {
     internal class Adattar
     {
+        List<NovenyFaj> novenyek;
+        public Adattar()
+        {
+            novenyek = new List<NovenyFaj>();
+        }
+
+        public void UjNoveny(NovenyFaj noveny)
+        {
+            novenyek.Add(noveny);
+        }
+
+        public void NovenyekListazasa()
+        {
+            foreach(var item in novenyek)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
     }
 }
